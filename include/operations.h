4c7b955a68ac756782eef88f5fb3990d39bbcfec
SantiagoLizBen <A01256357@tec.mx>
2025-11-26 10:56:17 -0700

    Add the other operations functions (multiplication and division)

--- include/operations.h
+++ include/operations.h
@@ -1,2 +1,4 @@
 double add(double a, double b);
 double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
--- src/operations.cpp
+++ src/operations.cpp
@@ -1,3 +1,11 @@
 #include "operations.h"
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
+double multiply(double a, double b) { return a * b; }
+double divide(double a, double b) {
+    if (b != 0) return a / b;
+    else {
+        cout << "Error: División por cero\n";
+        return 0;
+    }
+}
