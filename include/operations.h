6ace26ddc0a12797ccb778d8900748f653937df3
B0r34li5 <parisestebanv@gmail.com>
2025-11-26 15:50:56 -0700

    Operación adicional
    Se agregó la operacion "modulus" a operations.h y operations.cpp

--- include/operations.h
+++ include/operations.h
@@ -1,2 +1,3 @@
 double add(double a, double b);
 double subtract(double a, double b);
+double modulus(int a, int b);
--- src/operations.cpp
+++ src/operations.cpp
@@ -1,3 +1,4 @@
 #include "operations.h"
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
+double modulus(int a, int b) { return a % b; }
