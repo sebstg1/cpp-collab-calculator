e0d573036adb36372de4c3d7e298d48ad8965d07
sebstg1 <sebastiantanori8@gmail.com>
2025-11-26 22:33:55 -0700

    New operation power added

--- include/operations.h
+++ include/operations.h
@@ -2,3 +2,4 @@ double add(double a, double b);
 double subtract(double a, double b);
 double multiply(double a, double b);
 double divide(double a, double b);
+double power(double a, double b);
--- src/operations.cpp
+++ src/operations.cpp
@@ -1,11 +1,21 @@
-#include "operations.h"
+#include "../include/operations.h"
+#include <iostream>
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
 double multiply(double a, double b) { return a * b; }
 double divide(double a, double b) {
     if (b != 0) return a / b;
     else {
-        cout << "Error: División por cero\n";
+        std::cout << "Error: División por cero\n";
+        return 0;
+    }
+}
+double power(double a, double b) {
+    if (!(a == 0 && b == 0)){
+        for (int i = 0; i < b; i++) a *= a;
+    }
+    else {
+        std::cout << "Error: 0^0 no esta definido\n";
         return 0;
     }
 }
